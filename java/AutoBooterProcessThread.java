8d97a6b5dbc7b3756e783d574799f6b55db8719d
Chris Mottram <C.Mottram@ljmu.ac.uk>
2005-12-06 17:16:53 +0000

    Fix comments.

--- java/AutoBooterProcessThread.java
+++ java/AutoBooterProcessThread.java
@@ -1,5 +1,5 @@
 // AutoBooterProcessThread.java
-// $Header: /home/cjm/cvs/autobooter/java/AutoBooterProcessThread.java,v 0.4 2005-12-06 17:09:03 cjm Exp $
+// $Header: /home/cjm/cvs/autobooter/java/AutoBooterProcessThread.java,v 0.5 2005-12-06 17:16:53 cjm Exp $
 
 import java.lang.*;
 import java.io.*;
@@ -11,14 +11,14 @@ import java.util.*;
  * It re-spawns the process until the retry-count is exhausted in the retry-time, or the
  * process returns an engineering return status.
  * @author Chris Mottram
- * @version $Revision: 0.4 $
+ * @version $Revision: 0.5 $
  */
 public class AutoBooterProcessThread extends Thread
 {
 	/**
 	 * Revision Control System id string, showing the version of the Class.
 	 */
-	public final static String RCSID = new String("$Id: AutoBooterProcessThread.java,v 0.4 2005-12-06 17:09:03 cjm Exp $");
+	public final static String RCSID = new String("$Id: AutoBooterProcessThread.java,v 0.5 2005-12-06 17:16:53 cjm Exp $");
 	/**
 	 * The name of the process this thread is controlling.
 	 */
@@ -141,7 +141,7 @@ public class AutoBooterProcessThread extends Thread
 	 * @see AutoBooterStreamThread
 	 * @see AutoBooterProcessStatusInterface#getRetryTime
 	 * @see AutoBooterProcessStatusInterface#getReSpawnStatus
-	 * @see AutoBooterProcessStatusInterface#getEngineeringStatus
+	 * @see AutoBooterProcessStatusInterface#isEngineeringStatus
 	 */
 	public void run()
 	{
@@ -222,6 +222,10 @@ public class AutoBooterProcessThread extends Thread
 }
 //
 // $Log: not supported by cvs2svn $
+// Revision 0.4  2005/12/06 17:09:03  cjm
+// Swapped getEngineeringStatus for isEngineeringStatus, to allow
+// multiple process return values to stop re-spawning a process.
+//
 // Revision 0.3  2004/03/05 15:23:03  cjm
 // Added logging.
 //
